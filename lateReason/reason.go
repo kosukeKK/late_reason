71e731533815838189b727b5582a972ecc8626b0
kosukeKK <kosuke.kato@jmty.jp>
2018-01-02 20:36:33 +0900

    直した

--- lateReason/reason.go
+++ lateReason/reason.go
@@ -3,25 +3,21 @@ package lateReason
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	mds "github.com/kiji/late_reason/models"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"log"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"math/rand"
+	mds "github.com/kiji/late_reason/models"
+	dbInfo "github.com/kiji/late_reason/db"
 )
 
 var db *gorm.DB
 func Handler(c *gin.Context) {
-	db, err := gorm.Open("postgres","user=go_user dbname=ratereason sslmode=disable")
-	if err != nil {
-		log.Print("no connet")
-	}
+	db = dbInfo.DBconnect()
 	defer db.Close()
 	var (
 		reason = []mds.LateReason{}
 		jsonMap map[string]interface{} = make(map[string]interface{})
 		count = 0
 	)
-
 	db.Find(&reason).Count(&count)
 	jsonMap["reason"] = reason[rand.Intn(count)].Text
 	c.JSON(200, jsonMap)
